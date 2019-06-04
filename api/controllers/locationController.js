ce68660d814887d93ec09bf797a16c3abd12f016
jmnelson12 <jacob_m_nelson@live.com>
2019-06-03 22:00:39 -0500

    updated zip codes api (grabs all zip codes within polygon - on create, update, and delete)

--- api/controllers/locationController.js
+++ api/controllers/locationController.js
@@ -3,34 +3,6 @@ const { callToFreeMapTools } = require("../../lib/freeMapTools");
 module.exports = {
     getZipCodes: async (req, res) => {
         const { kmlString } = req.query;
-        // return await callToFreeMapTools(kmlString);
-        let grabZips = await callToFreeMapTools(kmlString);
-
-        console.log("-----");
-        console.log(grabZips);
-        console.log("-----");
-
-        return res.send({
-            success: true,
-            message: "Success",
-            payload: { a: "oi" }
-        });
-
-        // try {
-        //     console.log("before");
-
-        //     let req = await axios.get(
-        //         `${baseUrl}?kmlcoordinates=${kmlString}&rn=${rn}`,
-        //         {
-        //             cancelToken: new CancelToken(function executor(c) {
-        //                 cancelRequest = c;
-        //             }),
-        //             responseType: "xml"
-        //         }
-        //     );
-
-        //     console.log("after");
-        //     return req;
-        // } catch (e) {}
+        return res.send(await callToFreeMapTools(kmlString));
     }
 };
