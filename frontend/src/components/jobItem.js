bd95a6bab2e100b9d30f3a947a90f1ea6fab293b
jmnelson12 <jacob_m_nelson@live.com>
2019-07-11 23:12:57 -0500

    added job item component

---
+++ frontend/src/components/jobItem.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const JobItem = ({ jobData }) => {
+    return (
+        <div className="jobs-wrapper">
+            <div className="site-info-wrapper">
+                <div className="logo-wrapper">
+                    <a href={jobData.siteData.url}>
+                        <img
+                            src={jobData.siteData.icon}
+                            alt="indeed"
+                            className="logo"
+                        />
+                    </a>
+                </div>
+            </div>
+            <div className="job-listings-wrapper">
+                <div className="total-site-jobs">
+                    <p className="site-jobs-found">{jobData.jobCount}</p>
+                    <ul className="job-listings">
+                        {jobData.jobListings.map((job, key) => (
+                            <li className="listing" key={key}>
+                                <div className="listing-titile-wrapper">
+                                    <a
+                                        href={job.href}
+                                        rel="noopener noreferrer"
+                                        target="_blank">
+                                        <h4 className="listing-title">
+                                            {job.title}
+                                        </h4>
+                                    </a>
+                                    {job.company && (
+                                        <p className="job-company">
+                                            {job.company}
+                                        </p>
+                                    )}
+                                    {job.location && (
+                                        <p className="job-location">
+                                            {job.location}
+                                        </p>
+                                    )}
+                                    {job.salary && (
+                                        <p className="job-salary">
+                                            {job.salary}
+                                        </p>
+                                    )}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default JobItem;
